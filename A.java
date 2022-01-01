411ad76ed5090d3991fc7cf249929a8fbeeffb08
MuhammadShahriar <37985651+MuhammadShahriar@users.noreply.github.com>
2022-01-01 14:40:24 +0600

    Initial commit

---
+++ A.java
@@ -0,0 +1,7 @@
+
+class A {
+    public static void main(String [] args) {
+        System.out.println("This class is for testing and practicing");
+
+    }
+}
---
+++ FactoryDesignPattern/FactoryPatternDemo.java
@@ -0,0 +1,15 @@
+package FactoryDesignPattern;
+
+public class FactoryPatternDemo {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+        Shape shapeCircle = shapeFactory.getShape("circle");
+        Shape shapeRectAngle = shapeFactory.getShape("rectangle");
+        Shape shapeSquare = shapeFactory.getShape("Square");
+
+        shapeCircle.draw();
+        shapeRectAngle.draw();
+        shapeSquare.draw();
+    }
+}
